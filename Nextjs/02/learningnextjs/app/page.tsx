f840d59803a87330f7249507501c8e31c63bf3e8
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2026-07-24 16:28:41 +0545

    coding

--- Nextjs/02/learningnextjs/app/page.tsx
+++ Nextjs/02/learningnextjs/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans text-amber-50">
       <PostPage/>
+      
     </div>
   );
 }
